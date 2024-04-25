8c2fd795abc064bed38f301819745840a45b62e1
Ahmed Husain <ahmedhusain@Ahmeds-MacBook-Pro.local>
2024-04-25 13:23:48 -0700

    - added rings to show isha/fajr times - using dots to represent beginning/end of prayers

--- script.js
+++ script.js
@@ -4,18 +4,12 @@ const UI = {
   second: document.querySelector('.hand--second'),
   minute: document.querySelector('.hand--minute'),
   hour: document.querySelector('.hand--hour'),
-  // fajr: document.querySelector('.fajr'),
-  dhuhr: document.querySelector('.dhuhr'),
+
   dhuhr_arc: document.querySelector('.dhuhr-arc'),
-
-  asr: document.querySelector('.asr'),
   asr_arc: document.querySelector('.asr-arc'),
-  
-  maghrib: document.querySelector('.maghrib'),
   maghrib_arc: document.querySelector('.maghrib-arc'),
-
-  isha: document.querySelector('.isha'),
-  isha_arc: document.querySelector('.isha-arc')
+  isha_arc: document.querySelector('.isha-arc'),
+  fajr_arc: document.querySelector('.fajr-arc')
 }
 
 function timeToDegrees(time) {
@@ -23,7 +17,7 @@ function timeToDegrees(time) {
   const [hours, minutes] = time.split(':').map(Number);
 
   // Calculate degrees for hour hand
-  const hourDegrees = (hours % 12) * 30 + (minutes / 2);
+  const hourDegrees = hours * 30 + (minutes / 2);
 
   return hourDegrees;
 };
@@ -41,8 +35,11 @@ function describeArc(x, y, radius, startAngle, endAngle){
 
     var start = polarToCartesian(x, y, radius, startAngle);
     var end = polarToCartesian(x, y, radius, endAngle);
+    var angleDiff = endAngle - startAngle
+    
+    if (angleDiff < 0) angleDiff = 360 + angleDiff%360
 
-    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+    var largeArcFlag = angleDiff > 180 ? 1 : 0;
 
     var d = [
         "M", start.x, start.y, 
@@ -58,10 +55,12 @@ const updateClock = () => {
   // const date = now.getDate();
   const seconds = (now.getSeconds() + now.getMilliseconds() / 1000) / 60 * 360;
   const minutes = (now.getMinutes() + now.getSeconds() / 60) / 60 * 360;
-  const hours = (now.getHours()%12 + now.getMinutes() / 60) / 12 * 360;
+  const hours = (now.getHours() + now.getMinutes() / 60) / 12 * 360;
 
   const prayTimes = new PrayTimes('ISNA').getTimes(new Date(), [37.8, -122])
 
+  const fajr = timeToDegrees(prayTimes['fajr'])
+  const sunrise = timeToDegrees(prayTimes['sunrise'])
   const dhuhr = timeToDegrees(prayTimes['dhuhr'])
   const asr = timeToDegrees(prayTimes['asr'])
   const maghrib = timeToDegrees(prayTimes['maghrib'])
@@ -73,26 +72,25 @@ const updateClock = () => {
   UI.minute.style.transform = `rotate(${minutes}deg)`;
   UI.hour.style.transform = `rotate(${hours}deg)`;
 
-  UI.dhuhr.style.transform = `rotate(${dhuhr}deg)`;
-  UI.asr.style.transform = `rotate(${asr}deg)`;
-  UI.maghrib.style.transform = `rotate(${maghrib}deg)`;
-  UI.isha.style.transform = `rotate(${isha}deg)`;
+  const biggerRadius = 180;
+  const smallerRadius = 145;
 
-  UI.dhuhr_arc.setAttribute("d", describeArc(0, 0, 145, dhuhr, asr));
-  UI.asr_arc.setAttribute("d", describeArc(0, 0, 145, asr, maghrib));
-  UI.maghrib_arc.setAttribute("d", describeArc(0, 0, 145, maghrib, isha));
-  UI.isha_arc.setAttribute("d", describeArc(0, 0, 145, isha, dhuhr));
+  UI.dhuhr_arc.setAttribute("d", describeArc(0, 0, smallerRadius, dhuhr, asr));
+  UI.asr_arc.setAttribute("d", describeArc(0, 0, smallerRadius, asr, maghrib));
+  UI.maghrib_arc.setAttribute("d", describeArc(0, 0, smallerRadius, maghrib, isha));
+  UI.isha_arc.setAttribute("d", describeArc(0, 0, biggerRadius, isha, fajr));
+  UI.fajr_arc.setAttribute("d", describeArc(0, 0, biggerRadius, fajr, sunrise));
 
-  if (dhuhr < hours && hours < asr) {
-    UI.dhuhr_arc.style.stroke = "var(--red)";
-    UI.dhuhr_arc.style.strokeWidth = "3";
-  } else if (asr < hours && hours < maghrib) {
-    UI.asr_arc.style.stroke = "var(--red)";
-    UI.asr_arc.style.strokeWidth = "3";
-  } else if (maghrib < hours && hours < isha) {
-    UI.maghrib_arc.style.stroke = "var(--red)";
-    UI.maghrib_arc.style.strokeWidth = "3";
-  }
+  // if (dhuhr < hours && hours < asr) {
+    // UI.dhuhr_arc.style.stroke = "var(--red)";
+    // UI.dhuhr_arc.style.strokeWidth = "2";
+  // } else if (asr < hours && hours < maghrib) {
+    // UI.asr_arc.style.stroke = "blue";
+    // UI.asr_arc.style.strokeWidth = "3";
+  // } else if (maghrib < hours && hours < isha) {
+    // UI.maghrib_arc.style.stroke = "brown";
+    // UI.maghrib_arc.style.strokeWidth = "3";
+  // }
   //  else if ()
 
   requestAnimationFrame(updateClock)
