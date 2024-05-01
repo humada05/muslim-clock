d31a19916ac047331b21af83b7fd001ddc29d350
Ahmed Husain <ah27182@gmail.com>
2024-05-01 14:02:28 -0700

    - changed opacity for non-active prayers

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function timeToDegrees(time) {
 };
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+  var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
 
   return {
     x: centerX + (radius * Math.cos(angleInRadians)),
@@ -39,28 +39,28 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   };
 }
 
-function describeArc(x, y, radius, startAngle, endAngle){
+function describeArc(x, y, radius, startAngle, endAngle) {
 
-    var start = polarToCartesian(x, y, radius, startAngle);
-    var end = polarToCartesian(x, y, radius, endAngle);
-    var angleDiff = endAngle - startAngle
-    
-    if (angleDiff < 0) angleDiff = 360 + angleDiff%360
+  var start = polarToCartesian(x, y, radius, startAngle);
+  var end = polarToCartesian(x, y, radius, endAngle);
+  var angleDiff = endAngle - startAngle
 
-    var largeArcFlag = angleDiff > 180 ? 1 : 0;
+  if (angleDiff < 0) angleDiff = 360 + angleDiff % 360
 
-    var d = [
-        "M", start.x, start.y, 
-        "A", radius, radius, 0, largeArcFlag, 1, end.x, end.y
-    ].join(" ");
+  var largeArcFlag = angleDiff > 180 ? 1 : 0;
 
-    return d;       
+  var d = [
+    "M", start.x, start.y,
+    "A", radius, radius, 0, largeArcFlag, 1, end.x, end.y
+  ].join(" ");
+
+  return d;
 }
 
-function transformText(x, y, radius, startAngle, endAngle){
-  var centerAngle = (startAngle + endAngle)/2;
+function transformText(x, y, radius, startAngle, endAngle) {
+  var centerAngle = (startAngle + endAngle) / 2;
   var translationPoint = polarToCartesian(x, y, radius + 5, centerAngle);
-   return `translate(${translationPoint.x}, ${translationPoint.y}) rotate(${centerAngle})`;
+  return `translate(${translationPoint.x}, ${translationPoint.y}) rotate(${centerAngle})`;
 }
 
 const updateClock = () => {
@@ -88,7 +88,9 @@ const updateClock = () => {
   const biggerRadius = 180;
   const smallerRadius = 145;
 
-  const prayerTackerCoordinates = (isha < hours || hours < sunrise) ? polarToCartesian(0,0, biggerRadius, hours) : polarToCartesian(0,0, smallerRadius, hours);
+  const opacityAlpha = 0.3;
+
+  const prayerTackerCoordinates = (isha < hours || hours < sunrise) ? polarToCartesian(0, 0, biggerRadius, hours) : polarToCartesian(0, 0, smallerRadius, hours);
 
   UI.dhuhr_arc.setAttribute("d", describeArc(0, 0, smallerRadius, dhuhr, asr));
   UI.asr_arc.setAttribute("d", describeArc(0, 0, smallerRadius, asr, maghrib));
@@ -96,26 +98,30 @@ const updateClock = () => {
   UI.isha_arc.setAttribute("d", describeArc(0, 0, biggerRadius, isha, fajr));
   UI.fajr_arc.setAttribute("d", describeArc(0, 0, biggerRadius, fajr, sunrise));
 
-  UI.dhuhr_txt.setAttribute("transform", transformText(0,0,smallerRadius, dhuhr, asr));
-  UI.asr_txt.setAttribute("transform", transformText(0,0,smallerRadius, asr, maghrib));
-  UI.maghrib_txt.setAttribute("transform", transformText(0,0, smallerRadius, maghrib, isha));
-  UI.isha_txt.setAttribute("transform", transformText(0,0,biggerRadius, isha, fajr));
-  UI.fajr_txt.setAttribute("transform", transformText(0,0,biggerRadius, fajr, sunrise));
+  UI.dhuhr_txt.setAttribute("transform", transformText(0, 0, smallerRadius, dhuhr, asr));
+  UI.asr_txt.setAttribute("transform", transformText(0, 0, smallerRadius, asr, maghrib));
+  UI.maghrib_txt.setAttribute("transform", transformText(0, 0, smallerRadius, maghrib, isha));
+  UI.isha_txt.setAttribute("transform", transformText(0, 0, biggerRadius, isha, fajr));
+  UI.fajr_txt.setAttribute("transform", transformText(0, 0, biggerRadius, fajr, sunrise));
 
-  
   if (fajr < hours && hours < sunrise) {
     UI.fajr_arc.style.stroke = UI.fajr_txt.style.stroke = UI.fajr_txt.style.fill = "var(--secondary)";
+    UI.fajr_arc.style.opacity = UI.isha_txt.style.opacity = 1;
   } else if (dhuhr < hours && hours < asr) {
     UI.dhuhr_arc.style.stroke = UI.dhuhr_txt.style.stroke = UI.dhuhr_txt.style.fill = "var(--secondary)";
+    UI.dhuhr_arc.style.opacity = UI.dhuhr_txt.style.opacity = 1;
   } else if (asr < hours && hours < maghrib) {
     UI.asr_arc.style.stroke = UI.asr_txt.style.stroke = UI.asr_txt.style.fill = "var(--secondary)";
+    UI.asr_arc.style.opacity = UI.asr_txt.style.opacity = 1;
   } else if (maghrib < hours && hours < isha) {
     UI.maghrib_arc.style.stroke = UI.maghrib_txt.style.stroke = UI.maghrib_txt.style.fill = "var(--secondary)";
+    UI.maghrib_arc.style.opacity = UI.maghrib_txt.style.opacity = 1;
   } else if (isha < hours || hours < fajr) {
     UI.isha_arc.style.stroke = UI.isha_txt.style.stroke = UI.isha_txt.style.fill = "var(--secondary)";
+    UI.isha_arc.style.opacity = UI.isha_txt.style.opacity = 1;
   }
 
-  UI.prayer_tracker.setAttribute("transform",`translate(${prayerTackerCoordinates.x}, ${prayerTackerCoordinates.y})`)
+  UI.prayer_tracker.setAttribute("transform", `translate(${prayerTackerCoordinates.x}, ${prayerTackerCoordinates.y})`)
 
   requestAnimationFrame(updateClock)
 }
