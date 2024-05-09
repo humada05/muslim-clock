5e61341a5f38eca5a9976f4cb261cd3cd0d1bc71
Ahmed Husain <ah27182@gmail.com>
2024-05-08 22:40:40 -0700

    - make tracker transparent between fajr and dhuhr

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ const updateClock = () => {
 
   const opacityAlpha = 0.2;
 
-  const prayerTackerCoordinates = (isha < hours || hours < sunrise) ? polarToCartesian(0, 0, biggerRadius, hours) : polarToCartesian(0, 0, smallerRadius, hours);
+  const prayerTrackerCoordinates = (isha < hours || hours < sunrise) ? polarToCartesian(0, 0, biggerRadius, hours) : polarToCartesian(0, 0, smallerRadius, hours);
 
   // garbage code pleaaaase get rid of this 🙏
   UI.dhuhr_arc.setAttribute("d", describeArc(0, 0, smallerRadius, dhuhr, asr));
@@ -136,24 +136,28 @@ const updateClock = () => {
   UI.isha_arc.style.stroke = UI.isha_txt.style.stroke = UI.isha_txt.style.fill = "var(--highlight)";
   UI.isha_arc.style.opacity = UI.isha_txt.style.opacity = opacityAlpha;
 
-  if (fajr < hours && hours < sunrise) {
+  if (fajr <= hours && hours < sunrise) {
     UI.fajr_arc.style.stroke = UI.fajr_txt.style.stroke = UI.fajr_txt.style.fill = "var(--secondary)";
     UI.fajr_arc.style.opacity = UI.fajr_txt.style.opacity = 1;
-  } else if (dhuhr < hours && hours < asr) {
+  } else if (dhuhr <= hours && hours < asr) {
     UI.dhuhr_arc.style.stroke = UI.dhuhr_txt.style.stroke = UI.dhuhr_txt.style.fill = "var(--secondary)";
     UI.dhuhr_arc.style.opacity = UI.dhuhr_txt.style.opacity = 1;
-  } else if (asr < hours && hours < maghrib) {
+  } else if (asr <= hours && hours < maghrib) {
     UI.asr_arc.style.stroke = UI.asr_txt.style.stroke = UI.asr_txt.style.fill = "var(--secondary)";
     UI.asr_arc.style.opacity = UI.asr_txt.style.opacity = 1;
-  } else if (maghrib < hours && hours < isha) {
+  } else if (maghrib <= hours && hours < isha) {
     UI.maghrib_arc.style.stroke = UI.maghrib_txt.style.stroke = UI.maghrib_txt.style.fill = "var(--secondary)";
     UI.maghrib_arc.style.opacity = UI.maghrib_txt.style.opacity = 1;
-  } else if (isha < hours || hours < fajr) {
+  } else if (isha <= hours || hours < fajr) {
     UI.isha_arc.style.stroke = UI.isha_txt.style.stroke = UI.isha_txt.style.fill = "var(--secondary)";
     UI.isha_arc.style.opacity = UI.isha_txt.style.opacity = 1;
   }
 
-  UI.prayer_tracker.setAttribute("transform", `translate(${prayerTackerCoordinates.x}, ${prayerTackerCoordinates.y})`)
+  UI.prayer_tracker.setAttribute("transform", `translate(${prayerTrackerCoordinates.x}, ${prayerTrackerCoordinates.y})`)
+  
+  if (sunrise <= hours && hours < dhuhr) {
+    UI.prayer_tracker.setAttribute("fill", 'transparent');
+  }
 
   requestAnimationFrame(updateClock)
 }
